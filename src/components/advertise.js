141341965e07c5e4d74baaed143de4c17aee8f8a
Dreezy305 <bankoleidris@gmail.com>
2021-10-28 09:36:20 +0000

    advertisement

--- src/components/advertise.js
+++ src/components/advertise.js
@@ -1,15 +1,22 @@
 import React from "react";
 
 function Advertise() {
+  const advertArray = [
+    { id: 1, content: "ADVERTISEMENT" },
+    { id: 2, content: "ADVERTISEMENT" },
+    { id: 2, content: "ADVERTISEMENT" },
+  ];
   return (
     <section className="mt-5 pt-5">
-      <div className="container advert">
-        <div className="row">
-          <div className="card advert_card position-relative">
-            <div className="card-body">
-              <h1 className="text-center px-2">ADVERTISEMENT</h1>
+      <div className="container advert pb-3">
+        <div className="row justify-content-evenly">
+          {advertArray.map((item) => (
+            <div className="card advert_card position-relative w-25">
+              <div className="card-body">
+                <h1 className="text-center px-2">{item.content}</h1>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
