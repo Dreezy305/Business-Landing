028a4276a752e93ac721a9a80c54e801b87831d2
Dreezy305 <bankoleidris@gmail.com>
2021-10-28 08:42:19 +0000

    companies

--- src/components/company.js
+++ src/components/company.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Indeed from "../assets/1.png";
-//import Go from "../assets/godaddy.svg";
+import GoDaddy from "../assets/2.png";
+import Monster from "../assets/3.png";
+import eBay from "../assets/4.png";
+import Linkedln from "../assets/5.png";
 
 function Company() {
+  const logos = [
+    { id: 1, logo: eBay },
+    { id: 2, logo: Linkedln },
+    { id: 3, logo: Indeed },
+    { id: 4, logo: GoDaddy },
+    { id: 5, logo: Monster },
+  ];
   return (
     <section>
       <div className="container company">
-        <div className="row company_box">
-          <div class="col-md-4 col-lg-2">
-            <img src={Indeed} className="" alt="..." />
-          </div>
+        <div className="row company_box align-items-baseline justify-content-evenly mt-2 pb-2">
+          {logos.map((item) => (
+            <div class="col-md-4 col-lg-2">
+              <img src={item.logo} className="" alt="..." />
+            </div>
+          ))}
         </div>
       </div>
     </section>
