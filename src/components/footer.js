c6bba731d9c29500ac488a8c5afcbe26c9c05361
Dreezy305 <bankoleidris@gmail.com>
2021-10-29 10:12:17 +0000

    mobile responsiveness

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,51 +11,58 @@ function Footer() {
   ];
   return (
     <section className="footer mt-5 pt-5">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 footer_col1 pt-5 pb-3">
+      <div className="container ">
+        <div className="row footer_row">
+          <div className="col-md-6 footer_col1 pt-5 pb-4">
             <p className="py-5">
               This platform for minority groups to express their opinions on
               employment, workplace experiences and rate employers.{" "}
             </p>
-            <div className="d-flex flex-row pb-3 mb-3 footer_socials">
-              {social.map((item) => (
-                <img
-                  src={item.icon}
-                  className="p-3 me-2"
-                  alt="...."
-                  style={{ ...item.styles }}
-                />
-              ))}
-            </div>
-            <p className="footer_col1--p footer_copy mb-2">
+
+            <p className="footer_col1--p footer_copy">
               <span className="me-1">&copy;</span>
               <span className="me-1">Copyright</span>
               <span>{new Date().getFullYear()}</span>
             </p>
           </div>
-          <div className="col-md-2 footer_col2 pt-5">
+          <div className="col-lg-2 footer_col2 footer-subheader pt-5">
             <h5 className="mb-4">Company</h5>
-            <h6>About Us</h6>
-            <h6>Contact Us</h6>
-            <h6>Terms of Use</h6>
-            <h6>Privacy Policy</h6>
+            <ul class="list-inline company-list">
+              <li>About Us</li>
+              <li>Contact Us</li>
+              <li>Terms of Use</li>
+              <li>Privacy Policy</li>
+            </ul>
           </div>
-          <div className="col-md-2 footer_col2 pt-5">
+          <div className="col-lg-2 footer_col2 footer-subheader2 pt-5">
             <h5 className="mb-4">Features</h5>
-            <h6>Timeline</h6>
-            <h6>Peer Review</h6>
-            <h6>Create Account</h6>
-            <h6>Sign In</h6>
+            <ul class="list-inline company-list">
+              <li>Timeline</li>
+              <li>Peer Review</li>
+              <li>Create Account</li>
+              <li>Sign In</li>
+            </ul>
           </div>
-          <div className="col-md-2 footer_col2 pt-5">
+          <div className="col-lg-2 footer_col2 pt-5">
             <h5 className="mb-4">Contact Us</h5>
-            <h6>Inquiry@thaapp.com</h6>
-            <h6>1-800-200-300</h6>
-            <h6>54 Currah birin, Castleroy, Limerick</h6>
-            <h6>Ireland</h6>
+            <ul class="list-inline company-list">
+              <li>Inquiry@thaapp.com</li>
+              <li>1-800-200-300</li>
+              <li>54 Currah birin, Castleroy, Limerick</li>
+              <li>Ireland</li>
+            </ul>
           </div>
         </div>
+      </div>
+      <div className="d-flex flex-row footer_img pb-4 ">
+        {social.map((item) => (
+          <img
+            src={item.icon}
+            className="p-3 me-2"
+            alt="...."
+            style={{ ...item.styles }}
+          />
+        ))}
       </div>
     </section>
   );
