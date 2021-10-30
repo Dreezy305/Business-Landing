638b864c9b5f7f01f268364ebfcef2e2364aa114
Dreezy305 <bankoleidris@gmail.com>
2021-10-30 21:15:52 +0000

    stars rating

--- src/components/ratings.js
+++ src/components/ratings.js
@@ -2,7 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Ratings({ starObj, stars = 5 }) {
+function Ratings({
+  starObj,
+  stars = 5,
+  ratingValue,
+  fillColor,
+  emptyColor,
+  className,
+}) {
   const getRating = () => {};
   return <FontAwesomeIcon icon="star" />;
 }
@@ -10,10 +17,11 @@ function Ratings({ starObj, stars = 5 }) {
 export default Ratings;
 
 Ratings.propTypes = {
-  starObj: PropTypes.object.isRequired,
+  starObj: PropTypes.array,
   stars: PropTypes.number,
   ratingValue: PropTypes.number,
   fillColor: PropTypes.string,
   emptyColor: PropTypes.string,
   className: PropTypes.string,
+  size: PropTypes.string,
 };
